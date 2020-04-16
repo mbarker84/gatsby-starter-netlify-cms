1137185cc5260e6eebb1cadbb6c77401cd28bc65
Michelle Barker <mbarker_84@hotmail.co.uk>
2020-04-16 22:00:06 +0100

    404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,17 @@
-import React from 'react'
-import Layout from '../components/Layout'
+import React from "react";
+import { graphql, Link } from "gatsby";
+import Layout from "../components/Layout";
+import "../css/styles.scss";
 
 const NotFoundPage = () => (
   <Layout>
-    <div>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className="container section">
+      <h1 className="post__title">404: Page not found</h1>
+      <Link className="link link--secondary" to={`/`}>
+        Back to homepage
+      </Link>
     </div>
   </Layout>
-)
+);
 
-export default NotFoundPage
+export default NotFoundPage;
